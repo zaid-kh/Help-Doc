31fac514af465f66ccfabb68e953de60b7e13e02
ydaher97 <73422885+ydaher97@users.noreply.github.com>
2023-11-29 12:51:35 +0200

    routes

---
+++ src/components/Appointments/Appointments.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+const Appointments = () => {
+    const [doc,setDoc] = useState()
+
+  return (
+    <div>Appointments</div>
+  )
+}
+
+export default Appointments
