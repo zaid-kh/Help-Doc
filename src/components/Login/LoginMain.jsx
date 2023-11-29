876e83fa3ba49017b38883c301d93103b7629c88
jameelag1995 <agbariajameel@gmail.com>
2023-11-29 17:52:18 +0200

    Login & register

--- src/components/Login/LoginMain.jsx
+++ src/components/Login/LoginMain.jsx
@@ -3,10 +3,15 @@ import "./LoginMain.css";
 import LoginNav from "./LoginNav";
 import Login from "./Login";
 import Register from "./Register";
+import Navbar from "../Auth/Navbar";
+
 export default function LoginMain() {
+    const [isLogin,setIsLogin] = useState(true)
     return (
         <main className="login-register-container">
-            <Login />
+            <LoginNav isLogin={isLogin} setIsLogin={setIsLogin}  />
+            {/* <Navbar/> */}
+            {isLogin ? <Login  /> : <Register/>}
         </main>
     );
 }
