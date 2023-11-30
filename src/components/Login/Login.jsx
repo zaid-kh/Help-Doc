5eb2ef360774c83e656e6193e14dc2d49ab9fa07
zaid-kh <z.kh2800@gmail.com>
2023-11-30 14:05:22 +0200

    add user object to session and edit request is performed on logged in user

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,66 +2,70 @@ import React, { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-    const url = "https://6566fd1464fcff8d730f82fe.mockapi.io/users";
-    const [usersData, setUsersData] = useState([]);
-    const email = useRef();
-    const password = useRef();
-    const navigate = useNavigate();
+  const url = "https://6566fd1464fcff8d730f82fe.mockapi.io/users";
+  const [usersData, setUsersData] = useState([]);
+  const email = useRef();
+  const password = useRef();
+  const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (email.current && password.current) {
-            const emailexist = usersData.find(
-                (user) => user.email === email.current.value
-            );
-            const passwordexist = usersData.find(
-                (user) => user.password === password.current.value
-            );
-            console.log(emailexist, passwordexist);
-            if (emailexist && passwordexist) {
-                navigate("/dashboard");
-            }
-        }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email.current && password.current) {
+      const emailexist = usersData.find(
+        (user) => user.email === email.current.value
+      );
+      const passwordexist = usersData.find(
+        (user) => user.password === password.current.value
+      );
+      console.log(emailexist, passwordexist);
+      if (emailexist && passwordexist) {
+        // save id from emailexist object (retrieved user object) to session
+        sessionStorage.setItem("user", JSON.stringify(emailexist));
+        navigate("/dashboard");
+      }
+    }
+  };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const result = await response.json();
+        console.log(result);
+        setUsersData(result);
+      } catch (error) {
+        console.log(error);
+      }
     };
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(url);
-                const result = await response.json();
-                console.log(result);
-                setUsersData(result);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    return (
-        <div className="login-form-container">
-            <h1>
-                Welcome <br />
-                Back
-            </h1>
+  return (
+    <div className="login-form-container">
+      <h1>
+        Welcome <br />
+        Back
+      </h1>
 
-            <form id="login-form" onSubmit={handleSubmit}>
-                <input
-                    ref={email}
-                    type="email"
-                    name="useremail"
-                    id="email-input"
-                    placeholder="Email"
-                />
-                <input
-                    ref={password}
-                    type="password"
-                    name="userpassword"
-                    id="password-input"
-                    placeholder="Password"
-                />
-                <a className="forgot-password" href="">Forgot your password?</a>
-                <input type="submit" value="Login" />
-            </form>
-        </div>
-    );
+      <form id="login-form" onSubmit={handleSubmit}>
+        <input
+          ref={email}
+          type="email"
+          name="useremail"
+          id="email-input"
+          placeholder="Email"
+        />
+        <input
+          ref={password}
+          type="password"
+          name="userpassword"
+          id="password-input"
+          placeholder="Password"
+        />
+        <a className="forgot-password" href="">
+          Forgot your password?
+        </a>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
 }
